48270c0ed64ef3ee4aa6fef3af24a10054f22502
enoch.maonga <enoch.maonga@gmail.com>
2022-07-25 12:02:20 +0300

    updating deparmentsDao.java

--- src/main/java/Dao/DepartmentsDao.java
+++ src/main/java/Dao/DepartmentsDao.java
@@ -2,30 +2,26 @@ package Dao;
 
 public class Sql2oDepartmentsDao {
     package dao;
-
-
-	import models.Departments;
+    import models.Departments;
 	import models.News;
 	import models.Users;
-
-
 	import java.util.List;
-
-
     public interface DepartmentsDao {
 
 
-        //create
+        //create a department
         void add(Departments department);
         void addUserToDepartment(Users user, Departments department);
         //read
 
 
         List<Departments> getAll();
+
         Departments findById(int id);
         List<Users> getAllUsersInDepartment(int department_id);
         List<News> getDepartmentNews(int id);
-        //update
+        //update news from the departments
+
         //delete
         void clearAll();
 
